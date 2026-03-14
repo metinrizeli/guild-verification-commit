d290e46850ad1807fcda639b5f5500177b432ed6
metinrizeli <metinrizeli34@hotmail.com>
2026-03-15 01:08:41 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:08:38.531Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-14T22:08:41.657Z
+Commit: Fix CSS styling bug on mobile devices
