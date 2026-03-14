68a873b4ca46230febfb7c88a1466ce9fa5686c7
metinrizeli <metinrizeli34@hotmail.com>
2026-03-15 01:08:31 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:08:28.836Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-14T22:08:31.228Z
+Commit: Add export functionality for reports
