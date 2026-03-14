ddf1fcfdbe3265750947263fdaa8375355840651
metinrizeli <metinrizeli34@hotmail.com>
2026-03-15 01:50:15 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:08:41.657Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T22:50:15.786Z
+Commit: Fix memory leak in event handlers
