f3831dfe072413e19742e72ad46b2dfbb0e4f1d1
metinrizeli <metinrizeli34@hotmail.com>
2026-03-15 02:03:58 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T23:03:53.927Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T23:03:58.567Z
+Commit: Fix broken links in documentation
